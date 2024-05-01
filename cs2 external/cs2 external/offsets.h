ced3e5951831c865420263cb8c5b91738cf5bdf7
boyo3473 <141408370+boyo3473@users.noreply.github.com>
2024-05-01 13:04:42 +0100

    Update offsets.h

--- cs2 external/cs2 external/offsets.h
+++ cs2 external/cs2 external/offsets.h
@@ -1,15 +1,24 @@
 #include <cstddef>
 namespace offsets {
-	constexpr std::ptrdiff_t Force_Jump = 24311152;
-	constexpr std::ptrdiff_t dwEntityList = 25960104;
-	constexpr std::ptrdiff_t dwLocalPlayerPawn = 24338888;
-	constexpr std::ptrdiff_t m_iIDEntIndex = 0x159C;
+	//Buttons.hpp
+	constexpr std::ptrdiff_t Force_Jump = 0x1736920;
+
+	//offsets.hpp
+	constexpr std::ptrdiff_t dwEntityList = 0x18C9E78;
+	constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x173D5A8;
+	constexpr std::ptrdiff_t dwViewAngles = 0x1937E50;
+
+
+	//client.dll
+	constexpr std::ptrdiff_t m_iIDEntIndex = 0x13B0;
 	constexpr std::ptrdiff_t m_iTeamNum = 0x3CB;
 	constexpr std::ptrdiff_t m_iHealth = 0x334;
 	constexpr std::ptrdiff_t fFlags = 0x3D4;
 	constexpr std::ptrdiff_t vecOrigin = 0x127C;
-	constexpr std::ptrdiff_t dwViewAngles = 26409232;
+	
+
+
+	//glow esp is patched
 	constexpr std::ptrdiff_t m_fl_detected_by_enemy_sensor_time = 5184;
-	constexpr std::ptrdiff_t bSpottedByMask = 0x1698 + 0xC;
 
 }
